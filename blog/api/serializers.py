2bc443ff679365d74df0721796746ba5a6c9d05c
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-02 12:59:02 +0400

    Blog Model Serialaizer created.

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -2,19 +2,27 @@ from rest_framework import serializers
 from blog.models import Blog
 
 
-class BlogSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
+# class BlogSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
     
-    def create(self, validated_data):
-        return Blog.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Blog.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.save()
+#         return instance
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Blog
+        exclude = ('id',)
+        # fields = '__all__'
